8c4fe33d1d0971de8e21a086b1431e039ce351e0
justinhwbhang <justinhwbhang@gmail.com>
2023-02-13 12:12:40 -0800

    merge

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,23 +1,29 @@
-
-const signupFormHandler = async function(event) {
+const signupFormHandler = async (event) => {
     event.preventDefault();
-  
-    const usernameEl = document.querySelector("#username-input-signup");
-    const passwordEl = document.querySelector("#password-input-signup");
-    fetch("/api/user", {
-      method: "post",
-      body: JSON.stringify({
-        username: usernameEl.value,
-        password: passwordEl.value
-      }),
-      headers: { "Content-Type": "application/json" }
-    })
-      .then(function() {
-        document.location.replace("/dashboard");
-      })
-      .catch(err => console.log(err));
-  };
-  
-  document
-    .querySelector("#signup-form")
-    .addEventListener("submit", signupFormHandler);
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            let jsonRes = await response.json();
+            if(jsonRes.errors) {
+                alert(jsonRes.errors[0].message);
+            } else if (jsonRes.message) {
+                alert(jsonRes.message);
+            }
+        }
+    }
+};
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
